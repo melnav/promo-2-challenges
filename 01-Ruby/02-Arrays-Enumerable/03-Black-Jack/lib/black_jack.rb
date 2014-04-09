0a37c0dabea1e7040aa06af0ca6acebfdb510fa6
Melanie Navarro <melanie.navarro.pro@gmail.com>
2014-04-09 17:44:42 +0200

    First attempt, solved it

--- 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/black_jack.rb
+++ 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/black_jack.rb
@@ -1,11 +1,16 @@
 def bank_score
   # TODO: Use Random to get a new random score
+  bank_score = rand(16..21)
+  return bank_score
 end
 
 def pick_card
   # TODO: Use Random to get a new random card
+  pick_card = rand (1..11)
+  return pick_card
 end
 
 def game_outcome(bank, score)
-  # TODO: Take the bank and the score and output an array containing the bank and then the score
+  # TODO: Take the bank and the score and output an array containing  and then the scorethe bank
+  tableau = [bank, score]
 end
--- 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/croupier.rb
+++ 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/croupier.rb
@@ -2,16 +2,38 @@ require_relative 'black_jack'
 
 def play_game
   #TODO: make the user play from terminal in a while loop that will stop when the user will not be asking for  a new card
+ player_score = 0
+ player_score = player_score + pick_card
+ return bank_definit_score = bank_score
+
+ while "Yes" do
 end
 
 def state_of_the_game(score, bank)
   # TODO: Returns custom message with player's score and bank's score
+  puts "ton score est #{player_score} et #{bank_definit_score}"
 end
 
 def asking_for_card?(score)
   # TODO: Ask for a card only if the score is less or equal to 21, then returns true or false according to the user's choice
+  if player_score <= 21
+    puts  "card?"
+    response = gets.chomp
+      if response="yes"
+      player_score = player_score + pick_card
+  else
+  end
+  puts "card?"
+  response = gets.chomp
+
+  else
+  end
 end
 
 def build_message_for(outcome)
   #TODO: return specific message depending on the game outcome (= bank's score and user's score)
 end
+
+
+
+  end
