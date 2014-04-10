177d6398aafde256552ffe4c12a1532bb54bd50c
Melanie Navarro <melanie.navarro.pro@gmail.com>
2014-04-10 13:38:00 +0200

    First attempt, solved it

--- 01-Ruby/03-Blocks/01-Enumerable/lib/enumerable.rb
+++ 01-Ruby/03-Blocks/01-Enumerable/lib/enumerable.rb
@@ -1,36 +1,57 @@
 def sum_odd_indexed(array)
-  # TODO: computes the sum of elements at odd indices (1, 3, 5, 7, etc.) 
+  # TODO: computes the sum of elements at odd indices (1, 3, 5, 7, etc.)
   #       You should make use Enumerable#each_with_index
+    sum = 0
+
+
+    array.each_with_index do |element, index|
+      if index.odd?
+        sum = sum + array[index]
+      end
+    end
+    sum
 end
- 
+
 def even_numbers(array)
   # TODO: Return the even numbers from a list of integers.
   #       You should use Enumerable#select
+  #=> [2, 4]
+  array.select { |num|  num.even?  }
 end
+
 
 def short_words(array, max_length)
   # TODO: Take and array of words, return the array of words not exceeding max_length characters
   #       You should use Enumerable#reject
+  array.reject {|string| max_length < string.length}
 end
- 
+
 def first_under(array, limit)
   # TODO: Return the first number from an array that is less than limit.
   #       You should use Enumerable#find
+  array.find { |i| i < limit }
 end
- 
+
 def add_bang(array)
   # TODO: Take an array of strings and return a new array with "!" appended to each string.
   #       You should use Enumerable#map
+  array.map { |string| string + "!" }
 end
- 
+
 def product(array)
   # TODO: Calculate the product of an array of numbers.
   #       You should use of Enumerable#reduce
+  array.reduce(1, :*)
 end
- 
+
 def sorted_pairs(array)
   # TODO: Reorganize an array into slices of 2 elements, and sort each slice alphabetically.
-  #       You should make use of Enumerable#each_slice
+  #       You should make use of Enumerable#
+  new_array = []
+  array.each_slice(2) do |slice|
+    new_array << slice.sort
+  end
+  return new_array
 end
 
 
