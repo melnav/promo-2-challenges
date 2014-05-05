9ce535d7822768f2146c9d7c038c4accf2536fc3
Melanie Navarro <melanie.navarro.pro@gmail.com>
2014-05-05 14:42:18 +0200

    exo finished

--- 04-Front-End/04-Javascript-jQuery/01-DOM-Manipulation/application.js
+++ 04-Front-End/04-Javascript-jQuery/01-DOM-Manipulation/application.js
@@ -8,47 +8,49 @@
 function run_challenges() {
 
   // Ex 1. Read the content of the email input
-  var email = null;  // TODO: replace null, keep the email variable.
+  var email = $("#email").val();  // TODO: replace null, keep the email variable.
 
 
 
   // Ex 2. Fill the content of the email input with your email
+  $("#email").val("melanie.navarro.pro@gmail.com");
 
 
 
   // Ex 3. Replace the email hint (next to the input) with 'This is my email now'
   //       The text should be emphasized using a <strong> tag
-
+  var new_email = $('#email-hint').html('<strong>This is my email now</strong>');
 
   // Ex 4. Add the .blue CSS class to the table header cells (th elements)
-
+  $('th').css({
+   color : 'blue'
+  });
 
 
   // Ex 5. Count the number of table body rows there are (team count!)
-  var team_count = 0;  // TODO: replace 0, keep the team_count variable.
+  var team_count = $("tbody > tr").length;  // TODO: replace 0, keep the team_count variable.
 
 
 
   // Ex 6. Say there is a 15th team added to the table.
   //       Add a row at the bottom, this new team should have zero points.
-
-
+  $('tbody').append("<tr><td>15</td><td>toto</td><td>0</td></tr></tbody>")
 
   // Ex 7. Write some code to sum all points given to all teams
   var sum = 0;  // TODO: replace 0 with your sum result, keep the sum variable.
-
+  $('tbody tr td:nth-child(3)').each(function(){ sum += parseInt(this.innerText);})
 
 
   // Ex 8. Change the background color of all table header cells to #DDF4FF
-
+  $('thead tr th').css({background: '#DDF4FF'});
 
 
   // Ex 9. Translate the #results list 20px downward (animation would be great ^_^)
-
+  $('#results').animate({top: '20px'}, 500);
 
 
   // Ex 10. Remove the "Email:" label from the DOM
-
+  $('label').remove();
 
 
   // Checking exercise answers. DO NOT MODIFY BELOW
