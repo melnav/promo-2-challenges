b6bd5da7f3a57a26369f0f2cc0b9cd928e03e429
Melanie Navarro <melanie.navarro.pro@gmail.com>
2014-05-05 16:24:09 +0200

    ok

--- 04-Front-End/04-Javascript-jQuery/02-Events-Animation/application.js
+++ 04-Front-End/04-Javascript-jQuery/02-Events-Animation/application.js
@@ -1,5 +1,14 @@
 /*
   *
   * TODO: add your code here!
-  *
+
+  *a mettre en haut de tous les fichiers !!!!!!!!!!!!!!!!!! ATTENTION !!!!!!!!!
+
 */
+
+$(document).ready(function() {
+  $( "#message" ).hide().slideDown("slow");
+  $( "#message" ).click(function() {
+    $(this).slideUp("slow");
+  });
+});
